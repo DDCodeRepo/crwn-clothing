4f3cecd8ec78b4ebf8e1b586ed2efe11bf8c5b61
Debjit <debjit8dhar@gmail.com>
2021-01-04 22:47:32 +0530

    added checkout page, checkout item component and update cart to use selectors from  reselect

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,6 +1,6 @@
 import { combineReducers } from "redux";
 
-import userReducer from "./users/user.reducer";
+import userReducer from "./user/user.reducer";
 import cartReducer from "./cart/cart.reducer";
 
 export default combineReducers({
